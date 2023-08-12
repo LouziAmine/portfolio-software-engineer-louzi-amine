442ddf0c5d6e826e2e9fdca1772a21220a2d2aa6
LouziAmine <louzi.amine.pro@gmail.com>
2023-08-12 20:58:22 +0100

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+//import './App.css';
 
-function App() {
+
+
+
+/*function App() {
   return (
     <div className="App">
      Louzi Amine
@@ -9,4 +12,24 @@ function App() {
   );
 }
 
+export default App;*/
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
+
 export default App;
+
